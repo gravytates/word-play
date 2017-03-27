bb5eabc0f9d98898a781bcd0b41e4b204b436f74
Grady Shelton and James Higgins <james@>
2017-03-27 16:56:58 -0700

    change .map to .filter to avoid undefined spaces in result

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,9 @@ $(document).ready(function() {
 
 // everything above here works well
 
-    var newSentences = sentences.map(function(sentence){
-      if (sentence.length >= 3) {
-        return sentence
+    var newSentences = sentences.filter(function(sentence) {
+      if (sentence.length > 2) {
+        return sentence;
       }
     });
 
